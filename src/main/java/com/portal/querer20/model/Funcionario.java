87df78547397a1579763a9330a783b741155f262
Guilherme <guilherme.albuquerque.800@gmail.com>
2022-11-16 22:54:16 -0300

    Projeto final Ada e Bradesco Upgrade.Dev

--- src/main/java/com/portal/querer20/model/Funcionario.java
+++ src/main/java/com/portal/querer20/model/Funcionario.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "Funcionario")
 public class Funcionario {
 
     @Id
--- src/main/java/com/portal/querer20/model/Localidade.java
+++ src/main/java/com/portal/querer20/model/Localidade.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "Localidade")
 public class Localidade {
 
     @Id
